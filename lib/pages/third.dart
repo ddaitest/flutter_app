3d0dcc4d243876c049cc90f941fc8a00049b7172
lichen <lichen@redefine.global>
2019-03-28 18:58:31 +0800

    lc change

--- lib/pages/third.dart
+++ lib/pages/third.dart
@@ -12,16 +12,17 @@ class ThirdTab extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Icon(
-                Icons.directions_car,
+//                Icons.airport_shuttle,
+                Icons.hot_tub,
                 size: 78.0,
                 color: Colors.blue,
               ),
               new FlatButton(
-                  onPressed: _launchURL,
-                  child: new Text(
-                      'https://www.baidu.com/',
-                      style: new TextStyle(color: Colors.black54)
-                  )
+                onPressed: _launchURL,
+                child: new Text(
+                  'https://www.baidu.com/',
+                  style: new TextStyle(color: Colors.black54),
+                ),
               )
             ],
           ),
@@ -30,12 +31,13 @@ class ThirdTab extends StatelessWidget {
     );
   }
 
-  _launchURL() async {
-    const url = 'https://www.baidu.com';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+}
+
+_launchURL() async {
+  const url = 'https://www.baidu.com';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
