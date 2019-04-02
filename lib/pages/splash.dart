8a5f4a4e1e2862dc6cd479d6651d82a392515f32
lichen <lichen@redefine.global>
2019-04-03 02:00:41 +0800

    lc changed

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,25 +1,36 @@
 import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_app/common/common.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/home.dart';
-import 'package:flutter_app/pages/first.dart';
-import 'package:flutter_app/pages/second.dart';
-import 'package:flutter_app/pages/third.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 
 class SplashPage extends StatefulWidget {
   @override
   SplashState createState() => SplashState();
 }
 
+
 // 闪屏展示页面，首次安装时展示可滑动页面，第二次展示固定图片
 class SplashState extends State<SplashPage> {
+  final splash = 'splash_key';
+
+
+  Future saveSplashDB() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      sharedPreferences.setInt(
+          splash, 1);
+    });
+  }
+  Future getSplashDB() async {
+    int _firstStart;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _firstStart = await prefs.getInt(splash);
+    return _firstStart;
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
       body: new Stack(
         children: <Widget>[
@@ -36,26 +47,48 @@ class SplashState extends State<SplashPage> {
                   height: 150.0,
                 ),
                 new Text(
-                  '无佣金,更快捷',
+                  '无佣金，更快捷 $getSplashDB',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 34.0,
                   ),
                 ),
-                new Text(
-                  '''平台不收取佣金
-                  更快捷的叫车方式''',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
+                Padding(
+                  padding: const EdgeInsets.only(top: 70.0),
+                  child: new Text(
+                    '最后一公里不再难',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 100.0),
+                  child: new MaterialButton(
+                    onPressed: (){
+                      saveSplashDB();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    child: new Text(
+                      '马上体验',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    color: Colors.white,
+                    splashColor: Colors.blue,
+                  ),
+                ),
               ],
             ),
-          )
+          ),
         ],
-
       ),
     );
   }
