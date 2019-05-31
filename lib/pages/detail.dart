aa3c28facf0ccf5b6921d96a5c3b5fdc284b6310
seven <lc8681@gmail.com>
2019-06-01 00:25:20 +0800

    增加remark为空时的转义

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -5,7 +5,6 @@ import 'package:flutter_app/common/ItemView2.dart';
 import 'package:flutter_app/common/common.dart';
 import 'package:flutter_app/common/utils.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:video_player/video_player.dart';
 
 const Color c4 = Color(0xFF13D3CE);
 const Color c5 = Color(0xFFFF7200);
@@ -56,7 +55,7 @@ class DetailState extends State<DetailPage> {
               Icons.access_time,
             ),
             _getContainer(
-              Text(widget.remark, style: mainTitleFont),
+              Text(_remarkmsg(), style: mainTitleFont),
               Icons.event_note,
             ),
             Padding(
@@ -81,6 +80,17 @@ class DetailState extends State<DetailPage> {
         ));
   }
 
+  ///remark为空时转义成"无"
+  _remarkmsg() {
+    switch (widget.remark) {
+      case (''):
+        return '无';
+      default:
+        return widget.remark;
+    }
+  }
+
+  ///判断是否有广告数据才展示
   _adJudge() {
     if (adForDetailUrl != null) {
       return Padding(
@@ -100,6 +110,7 @@ class DetailState extends State<DetailPage> {
     }
   }
 
+  ///list中的通用样式
   Widget _getContainer(var title, IconData leadIcon, {IconData trailIcon}) {
     return Container(
         padding: const EdgeInsets.all(5),
@@ -110,11 +121,12 @@ class DetailState extends State<DetailPage> {
         ));
   }
 
+  ///位置样式
   _getRoadLine() {
     return Column(
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.only(bottom: 5),
+          padding: const EdgeInsets.only(top: 2, bottom: 2),
           child:
               _getInfoView(Icons.trip_origin, c4, widget.from, mainTitleFont),
         ),
